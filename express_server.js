c30efc5311e42477cad986ca8291677cfc194e3a
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-12-06 00:08:33 +0000

    Added body-parser as middleware to read input from forms

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const app = express();
 const PORT = 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -23,6 +26,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req,res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
+});
+
+app.get("/urls/new", (req,res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
